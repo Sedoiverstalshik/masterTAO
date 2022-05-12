369acabd8a109324295ba532ff3e5dbb228da9f4
Sedoiverstalshik <104726458+Sedoiverstalshik@users.noreply.github.com>
2022-05-12 16:48:30 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -27,6 +27,23 @@ const delivery = new Swiper('.delivery__swiper', {
     nextEl: '.delivery__swiper-next, .swiper-button-next',
     prevEl: '.delivery__swiper-prev, .swiper-button-prev',
   },
+  pagination: {
+    el: '.delivery__swiper-pagination, .swiper-pagination',
+  },
+
+  breakpoints: {
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    300: {
+      slidesPerView: 1,
+    },
+  }
 });
 const services = new Swiper('.services__swiper', {
   slidesPerView: 3,
@@ -37,4 +54,54 @@ const services = new Swiper('.services__swiper', {
     nextEl: '.services__swiper-next, .swiper-button-next',
     prevEl: '.services__swiper-prev, .swiper-button-prev',
   },
+  pagination: {
+    el: '.delivery__swiper-pagination, .swiper-pagination',
+  },
+
+  breakpoints: {
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    300: {
+      slidesPerView: 1,
+    },
+  }
 });
+
+// modal
+const modal = document.querySelectorAll('.modal__form')
+// const modalVolume = document.querySelector('.form__volume')
+const modalBtn = document.querySelectorAll('.btn__accent')
+const modalBtnCalc = document.querySelector('.btn__calculate')
+const close = document.querySelectorAll('.form__close')
+
+// modal.forEach(function (e) {
+//   e.addEventListener('click', function () {
+//     modal.style.display = 'block';
+//   })
+// });
+
+// modalBtn.forEach(function (i) {
+//   i.addEventListener('click', function () {
+//     modal.classList.togle('open')
+//   })
+// });
+
+
+
+
+// modalBtnCalc.addEventListener('click', () => {
+//   modalVolume.style.display = 'grid';
+// });
+
+
+// close.forEach(function (e) {
+//   e.addEventListener('click', function () {
+//     modal.style.display = 'none';
+//   })
+// });
